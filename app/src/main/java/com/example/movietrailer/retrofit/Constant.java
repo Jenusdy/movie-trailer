3404e0f642f90db0fb448230e535954456a87ea2
Fawcet Jenusdy Makay <jenusdy@gmail.com>
2023-04-02 13:21:47 +0700

    Ignore Constant

--- app/src/main/java/com/example/movietrailer/retrofit/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.example.movietrailer.retrofit;
-
-public class Constant {
-    public static String BASE_URL = "https://api.themoviedb.org/3/";
-    public static String API_KEY = "";
-    public static String LANGUAGE = "en-US";
-    public static String IMAGE_PATH = "https://www.themoviedb.org/t/p/original/";
-}
